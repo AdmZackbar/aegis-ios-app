50df5e35868e636767ca1473aa074bf5a377a735
Zach Wassynger <zach.wassynger@gmail.com>
2026-04-21 10:10:38 -0500

    feat: edit/delete support for ExpenseTags

--- AegisTracker/View/ExpenseTagsView.swift
+++ AegisTracker/View/ExpenseTagsView.swift
@@ -14,7 +14,10 @@ struct ExpenseTagsView: View {
     @Query(sort: \ExpenseTag.name) var tags: [ExpenseTag]
     
     @State private var showAdd = false
+    @State private var showEdit = false
+    @State private var showDelete = false
     @State private var name = ""
+    @State private var editTag: ExpenseTag? = nil
     
     var body: some View {
         List(tags) { tag in
@@ -23,6 +26,14 @@ struct ExpenseTagsView: View {
             } label: {
                 tagView(tag)
             }.buttonStyle(.plain)
+                .contextMenu {
+                    editButton(tag)
+                    deleteButton(tag).tint(.red)
+                }
+                .swipeActions {
+                    deleteButton(tag).tint(.red)
+                    editButton(tag).tint(.blue)
+                }
         }.navigationTitle("View Expense Groups")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
@@ -36,15 +47,59 @@ struct ExpenseTagsView: View {
             }
             .alert("Add New Group:", isPresented: $showAdd) {
                 TextField("Name", text: $name)
+                Button("Cancel") {
+                    showAdd = false
+                }
                 Button("Add") {
                     modelContext.insert(ExpenseTag(name: name))
                     name = ""
                     showAdd = false
                 }.disabled(isNameInvalid())
-                Button("Cancel") {
-                    showAdd = false
-                }
             }
+            .alert("Edit Group Name:", isPresented: $showEdit) {
+                TextField("Name", text: $name)
+                Button("Cancel") {
+                    showEdit = false
+                }
+                Button("Save") {
+                    if let tag = editTag {
+                        withAnimation {
+                            tag.name = name
+                        }
+                    }
+                    name = ""
+                    showEdit = false
+                }.disabled(isNameInvalid())
+            }
+            .alert("Delete \(editTag?.name ?? "tag")?", isPresented: $showDelete) {
+                Button("Delete", role: .destructive) {
+                    if let tag = editTag {
+                        withAnimation {
+                            modelContext.delete(tag)
+                        }
+                    }
+                    showDelete = false
+                }.disabled(editTag == nil)
+            }
+    }
+    
+    private func editButton(_ tag: ExpenseTag) -> some View {
+        Button {
+            name = tag.name
+            editTag = tag
+            showEdit = true
+        } label: {
+            Label("Edit Name", systemImage: "pencil.circle")
+        }
+    }
+    
+    private func deleteButton(_ tag: ExpenseTag) -> some View {
+        Button {
+            editTag = tag
+            showDelete = true
+        } label: {
+            Label("Delete", systemImage: "trash")
+        }
     }
 
     private func tagView(_ tag: ExpenseTag) -> some View {
